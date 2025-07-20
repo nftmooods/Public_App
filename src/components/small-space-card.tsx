c2a6afb0b0f2604cc4aa3c52628cb6e3710022d5
Mooods <Martin.lisen@gmail.com>
2025-07-20 14:40:55 +0000

    en mode fullview, quand on est au survol, il faut écrire l'heure en blan

--- src/components/small-space-card.tsx
+++ src/components/small-space-card.tsx
@@ -79,7 +79,7 @@ export function SmallSpaceCard({ space, displayTimezone }: SmallSpaceCardProps) 
     <div className={cn(cardClasses)}>
         <div className="flex flex-col text-foreground">
           <div className="flex justify-between items-center mb-1 flex-wrap gap-1">
-            <p className="text-muted-foreground text-xs whitespace-nowrap transition-all group-hover:font-medium group-hover:text-sm">{formattedTime}</p>
+            <p className="text-muted-foreground text-xs whitespace-nowrap transition-all group-hover:font-medium group-hover:text-sm group-hover:text-foreground">{formattedTime}</p>
           </div>
           <p className="font-semibold leading-tight transition-all text-primary group-hover:text-base">{space.name}</p>
         </div>
