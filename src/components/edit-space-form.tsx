7561a6447e97387d65d81ba0073a954c6fc810a5
Mooods <Martin.lisen@gmail.com>
2025-07-20 20:07:31 +0000

    réduit la taille de l'écriture de la phrase et utilise la meme pour le h

--- src/components/edit-space-form.tsx
+++ src/components/edit-space-form.tsx
@@ -246,7 +246,7 @@ export function EditSpaceForm({ space }: EditSpaceFormProps) {
                 <FormControl>
                     <Input {...field} disabled={!isSuperAdmin} />
                 </FormControl>
-                <FormDescription>
+                <FormDescription className="text-xs">
                     {isSuperAdmin ? "You can change the author name." : "The author of an event cannot be changed., contact nftmooods for a manual editing"}
                 </FormDescription>
                 <FormMessage />
@@ -262,7 +262,7 @@ export function EditSpaceForm({ space }: EditSpaceFormProps) {
                 <FormControl>
                     <Input {...field} disabled={!isSuperAdmin} />
                 </FormControl>
-                 <FormDescription>
+                 <FormDescription className="text-xs">
                     {isSuperAdmin ? "You can change the host name." : "The host of an event cannot be changed."}
                 </FormDescription>
                 <FormMessage />
