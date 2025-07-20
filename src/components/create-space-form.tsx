30502a17acc31a1676719df8c8a3d8f9bd1f8525
Mooods <Martin.lisen@gmail.com>
2025-07-20 20:25:28 +0000

    réduit la le menu creation l'esapce entre les élément pour optimiser aus

--- src/components/create-space-form.tsx
+++ src/components/create-space-form.tsx
@@ -169,7 +169,7 @@ export function CreateSpaceForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="name"
@@ -336,7 +336,7 @@ export function CreateSpaceForm() {
             )}
             />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <FormField
               control={form.control}
               name="startTime"
