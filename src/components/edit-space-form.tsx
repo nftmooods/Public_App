6d22e1b24b801c3d50be772c4e3d3078f11732fd
Mooods <Martin.lisen@gmail.com>
2025-07-20 20:24:05 +0000

    réduit la hauteur entre les différente partie pour mieux optimiser l'esp

--- src/components/edit-space-form.tsx
+++ src/components/edit-space-form.tsx
@@ -223,8 +223,8 @@ export function EditSpaceForm({ space }: EditSpaceFormProps) {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4">
             <FormField
               control={form.control}
               name="name"
@@ -298,7 +298,7 @@ export function EditSpaceForm({ space }: EditSpaceFormProps) {
             />
         </div>
         
-        <div className="space-y-6">
+        <div className="space-y-4">
             <FormField
               control={form.control}
               name="contentPlace"
@@ -373,7 +373,7 @@ export function EditSpaceForm({ space }: EditSpaceFormProps) {
             />
         </div>
         
-         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
+         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4">
             <FormField
                 control={form.control}
                 name="dayOfWeek"
@@ -479,7 +479,6 @@ export function EditSpaceForm({ space }: EditSpaceFormProps) {
                     <FormLabel className="text-base">
                     Show in Calendar
                     </FormLabel>
-                    
                 </div>
                 <FormControl>
                     <Switch
