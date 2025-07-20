0eb6b25f7a93c44f032b4a7d662656268d5b55b4
Mooods <Martin.lisen@gmail.com>
2025-07-20 19:05:36 +0000

    dans le menu timezone c'est à nouveau avec les appelation bizarre, utili

--- src/lib/timezones.ts
+++ src/lib/timezones.ts
@@ -5,8 +5,8 @@ export const timezones = [
     { value: "America/Chicago", label: "Chicago (CT)" },
     { value: "America/New_York", label: "New York (ET)" },
     { value: "UTC", label: "UTC" },
-    { value: "Europe/London", label: "London (GMT/BST)" },
-    { value: "Europe/Paris", label: "Paris (CET/CEST)" },
+    { value: "Europe/London", label: "London (GMT)" },
+    { value: "Europe/Paris", label: "Paris (CET)" },
     { value: "Asia/Tokyo", label: "Tokyo (JST)" },
-    { value: "Australia/Sydney", label: "Sydney (AEST/AEDT)" },
+    { value: "Australia/Sydney", label: "Sydney (AET)" },
 ];
